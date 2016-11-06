2bc474224dd63ff4db06dd3686a175878e771cae
Peter Prokop <pitrowech@gmail.com>
2016-11-06 10:42:44 +0100

    Update to Swift 3.0

--- Meditate/ViewController.swift
+++ Meditate/ViewController.swift
@@ -1,25 +1,67 @@
-//
-//  ViewController.swift
-//  Meditate
-//
-//  Created by Peter Prokop on 10/04/16.
-//  Copyright © 2016 Prokop. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var circleView: UIView!
+    @IBOutlet weak var rythmLabel: UILabel!
+    
+    var timer: NSTimer!
+    var scale = CGFloat(1)
+    var isInflating = true
+    
+    let timerInterval = 0.02
+    var cyclesPerMinute = CGFloat(5)
+    var speed: CGFloat { return 0.02 / (60 / self.cyclesPerMinute / 2) }
+    var firstLayout = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        timer = NSTimer.scheduledTimerWithTimeInterval(timerInterval, target: self, selector: #selector(timerCallback), userInfo: nil, repeats: true)
+        
+        updateRhytmLabel()
+    }
+    
+    func updateRhytmLabel() {
+        rythmLabel.text = "\(cyclesPerMinute) cycles/min"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func timerCallback() {
+        scale += isInflating ? speed : -speed
+        
+        if scale <= 0 {
+            scale = 0
+            isInflating = true
+        } else if scale >= 1 {
+            scale = 1
+            isInflating = false
+        }
+        
+        circleView.transform = CGAffineTransformMakeScale(scale, scale)
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        if firstLayout {
+            circleView.layer.cornerRadius = circleView.frame.width/2
+            circleView.layer.masksToBounds = true
+            firstLayout = false
+        }
+    }
+    
+    @IBAction func panGestureCallback(sender: UIPanGestureRecognizer) {
+        let translation = sender.translationInView(view).x / view.bounds.width
+        
+        if translation > 0.25 {
+            cyclesPerMinute += 0.25
+            sender.setTranslation(CGPoint.zero, inView: view)
+            updateRhytmLabel()
+        } else if translation < -0.25 {
+            cyclesPerMinute -= 0.25
+            sender.setTranslation(CGPoint.zero, inView: view)
+            updateRhytmLabel()
+        }
+    }
 }
 
